212f8bf866780b1e4819e6258b3ffbe132f84c48
Rai <raissasimpson@gmail.com>
2020-11-18 09:50:08 -0800

    Updated Portfolio

--- assignments/week4/express_server/index.js
+++ assignments/week4/express_server/index.js
@@ -9,10 +9,10 @@ const http = require("http").Server(app); // Link the http to the express module
 
 // Decide on the port number to listen on when running Express server
 // DO NOT USE 80, 443, 20, 21, 22
-const port = 3000; // Node uses 3000 (8080 also safe)
+const port = 3000; // Node uses 3000 (8080 also safe and public)
 
 // Tell http module that we will be listening on the number in the port module
-http.listen(port);
+http.listen(port); // Method to http:
 
 console.log("Express server is now running on " + port); //http://127.0.0.1:3000/ or localhost:3000
 
@@ -22,5 +22,5 @@ app.use("/", express.static("public_html/") );
 // A "second" website accessed only if you type localhost:3000/portfolio
 app.use("/portfolio", express.static("portfolio/"));
 
-
 // npm packages reminder: use .gitignore
+// Email POP3 or IMAP are protocols (services) on the website
