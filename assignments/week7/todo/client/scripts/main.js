95518e0740c42a1ab012ff77eb6aa8e9f78ad938
Rai <raissasimpson@gmail.com>
2020-12-09 15:41:02 -0800

    Added CSS

--- assignments/week7/todo/client/scripts/main.js
+++ assignments/week7/todo/client/scripts/main.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
 
     $("#submit").click(() => {
 
-        let priorityNumber = $("#notes-priority option:selected").attr("value");
+        let priorityNumber = $("#note-priority option:selected").attr("value");
 
         priorityNumber = parseInt(priorityNumber);
 
@@ -19,10 +19,26 @@ $(document).ready(() => {
             timestamp: Date.now()
         }
 
-      $.post(base_url + "newNote", noteObject, (data) => {
-          console.log(data.saved);
-      });
-    
+        $("#submit").attr("disabled", "disabled");
+        $("#loading-icon").fadeIn(600);
+        
+        $.post(base_url + "newNote", noteObject, (data) => {
+            console.log(data.saved);
+            if (data.saved === true) {
+                setTimeout(() => {
+                    $("#submit").removeAttr("disabled");
+                    $("#loading-icon").fadeOut(600);
+                    $("#submit-message").text("Successfuly saved the following: " + noteObject.title);
+                    $("#submit-message").fadeIn(600);
+                    setTimeout(() => {
+                        $("#submit-message").fadeOut(600, () => {
+                          $("#submit-message").text("");
+                        }); 
+                    }, 4000);
+                }, 2000);
+            }
+        });
+        
     });
-    
+
 });
--- assignments/week7/todo/index.js
+++ assignments/week7/todo/index.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const credentials = require("./credentials.js")
+const credentials = require("./credentials.js");
 
 const dbCredentials = credentials.dbURL;
-
-let dbOption = {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true};
-let dbConnection = mongoose.connect(dbCredentials, dbOption, (error) => {
+const dbOptions = {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true};
+let dbConnection = mongoose.connect(dbCredentials, dbOptions, (error) => {
     if (error) {
         console.log("Mongoose error: " + error);
-    } else{
+    } else {
         console.log("MongoDB connection opened.");
     }
 });
+
+let todoSchema = new mongoose.Schema({
+    title: String,
+    text: String,
+    completed: Boolean,
+    archived: Boolean,
+    priority: Number,
+    timestamp: Number
+});
+
+let todoModel = new mongoose.model("notecollections", todoSchema);
 
 const port = 3000;
 
@@ -21,21 +31,39 @@ const http = require("http").Server(app);
 http.listen(port);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 console.log("Express server is running on port " + port);
 
 app.use("/", express.static("client/"));
 
 app.post("/newNote", (request, response) => {
+    
     const note = request.body;
 
-    console.log(note);
+    // Sanitize Inputs
 
-    const responseObject = {
-        saved: true,
-        error: null
-    };
+    let newNoteDocument = new todoModel({
+        title: note.title,
+        text: note.text,
+        completed: (note.completed === "true"),
+        archived: (note.archived === "true"),
+        priority: parseInt(note.priority),
+        timestamp: parseInt(note.timestamp)
+    });
 
-    response.send(responseObject);
+    newNoteDocument.save((error) => {
+        
+        const responseObject = {
+            saved: false,
+            error: null
+        };
+
+        if (error) {
+            response.send(responseObject);
+        } else {
+            responseObject.saved = true;
+            response.send(responseObject);
+        }
+    });  
 });
