a74e7ae4ff9f6a47369f84610a2cf274a0731d6f
Rai <raissasimpson@gmail.com>
2020-12-17 11:40:47 -0800

    Added Tic Tac Toe

---
+++ assignments/week8/react/tutorial-app/src/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square" onClick={function () { alert ("clicked"); }}>
+          {this.props.value}
+        </button>
+      );
+    }
+  }
+  // React Components
+  class Board extends React.Component {
+    renderSquare(i) {
+      return <Square value ={i} />; // modified this line
+    }
+  
+    render() {
+      const status = 'Next player: X';
+  // JSX - like html but react converts it to javascript
+      return (
+        <div> 
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
